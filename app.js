693882900abd4919f7d675308eda0e3df623e509
GustavoHiguera <ghiguera@ucol.mx>
2022-02-06 16:39:41 -0600

    implemented refactors

--- app.js
+++ app.js
@@ -7,11 +7,29 @@
 let greet = () => console.log(`Hola`);
 //refactor de greet versión ES6, usando una arrow function.
 
+/*
 function logGreeting(fn) {
     fn();
 }
-logGreeting(greet);
+logGreeting(greet);`
+*/ //Versión inicial de logGreeting, sin las ventajas de ES6.
 
+let logGreeting = (fn) => fn();
+logGreeting(greet);
+//refactor de logGreeting, con una función flecha y de acuerdo a
+//las especificaciones de ES6.
+
+/*
+let greetMe = function () {
+    console.log('Hello from the function expression');
+}
+greetMe();
+*/ //Versión inicial de la función greetMe sin las ventajas de ES6.
+
+let greetMe = () => console.log(`Hello from the function expression`);
+greetMe();
+//refactor de greetMe, con una función flecha, con backticks en lugar de
+//las comillas simples y sin curly braces porque solo es una línea de código.
 
 let x=2;
 
@@ -33,5 +51,7 @@ logGreeting(function(){console.log(`Hola`)});
 
 logGreeting( () => {console.log(`Hola`);})
 //Versión 2, en ES6.
+
+
 
 
